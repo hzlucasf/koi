57c25b3aad6d8414fc19d5729db734c5800dcb8f
hzlucasf <hzlucasf@gmail.com>
2024-10-04 12:18:04 -0300

    Implemented koi_new_string

---
+++ koi.c
@@ -0,0 +1,45 @@
+#include "koi.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+u8 koi_last_err_code = KOI_NO_ERR;
+
+typedef struct String
+{
+    char* str;
+} String;
+
+String* koi_new_string(const char* str)
+{
+    koi_last_err_code = KOI_NO_ERR;
+
+    if (str == NULL)
+    {
+        koi_last_err_code = KOI_INVALID_ARGS_ERR;
+
+        return NULL;
+    }
+
+    String* s = malloc(sizeof(String));
+
+    if (s == NULL)
+    {
+        koi_last_err_code = KOI_MEM_ALLOC_ERR;
+
+        return NULL;
+    }
+
+    s->str = strdup(str);
+
+    if (s->str == NULL)
+    {
+        koi_last_err_code = KOI_MEM_ALLOC_ERR;
+
+        free(s);
+
+        return NULL;
+    }
+
+    return s;
+}
---
+++ koi.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+
+#define KOI_NO_ERR           0
+#define KOI_INVALID_ARGS_ERR 1
+#define KOI_MEM_ALLOC_ERR    2
+
+extern u8 koi_last_err_code;
+
+typedef struct String String;
+
+String* koi_new_string(const char*);
